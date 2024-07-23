995897410d993b89030335e627809179b536e76b
Arthur669 <120607127+Arthur669@users.noreply.github.com>
2024-07-23 19:44:33 -0400

    Update main.py
    foi adicionado 2 comentarios para facilitar a escolha do nome de usuario e senha

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-name_user = input("digite o nome de usuario:")
-if name_user == "nexusadmin/123":
+name_user = input("digite o nome de usuario:") 
+if name_user == "nexusadmin/123": #escolha isso "nexusadmin/123"
     print("acesso permitido continue o login")
 else:
     exit()
 password = input("digite a senha:")
-if password == "hash256//123":
+if password == "hash256//123": #escolha isso "hash256//123"
     print("acesso permitido")
 else:
     exit()
